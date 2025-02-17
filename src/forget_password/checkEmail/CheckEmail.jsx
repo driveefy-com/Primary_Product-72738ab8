2cc5d16003fe94324e0383a0c272750abda33e7c
Soudarjya <soudyg707@gmail.com>
2025-02-17 16:39:51 +0530

    changes_in_set_Password

--- src/forget_password/checkEmail/CheckEmail.jsx
+++ src/forget_password/checkEmail/CheckEmail.jsx
@@ -1,6 +1,27 @@
-
+import { Link } from "react-router-dom";
+import "./styles/CheckEmail.scss";
+import icon from '../../assets/icons/checkEmailIcon.svg'
+import { useTranslation } from "react-i18next";
 export const CheckEmail = () => {
+  const { t } = useTranslation();
   return (
-    <div>CheckEmail</div>
-  )
-}
+    <div className="check-email-main-container">
+      <div className="check-email-img-container"></div>
+      <div className="check-email-form-container">
+        <div className="check-email-text-container">
+        <h1>{t('checkEmail.title')}</h1>
+        <p>
+         {t('checkEmail.message')}
+        </p>
+        {/* <div className="check-email-logo-container"> */}
+          <img src={icon} alt="" />
+        {/* </div> */}
+        <h3 className="check-email-resend-email">
+         {t('checkEmail.resend')}
+          <Link to={'/'} className="Link"> {t('checkEmail.resendLink')}</Link>
+        </h3>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/forget_password/setNewPassword/SetNewPassword.jsx
+++ src/forget_password/setNewPassword/SetNewPassword.jsx
@@ -1,6 +1,29 @@
-
+import SubmitButton from '../../common/buttons/SubmitButton'
+import { FormComponents } from '../../common/form/FormComponents'
+import { setNewPassword } from '../../common/form/FormData'
+import './styles/SetNewPassword.scss'
 export const SetNewPassword = () => {
   return (
-    <div>SetNewPassword</div>
+    <div className="set-password-main-container">
+      <div className="set-password-img-container">
+      </div>
+      <div className="set-password-text-container">
+        <div className="set-password-form-container">
+          <h1>Set a new password</h1>
+          <p>Create a new password. Ensure it is different from the previous one for security</p>
+          <div className="set-password-input-container">
+            <form action="">
+            <h3>Password</h3>
+            <FormComponents formData={setNewPassword[0]}/>
+            <h3>Confirm Password</h3>
+            <FormComponents formData={setNewPassword[1]}/>
+            </form>
+            <div className="set-password-button-container">
+            <SubmitButton text={'Update Password'}/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
