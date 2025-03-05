c949dab1c3b00033b263b1e7c93cf0bad0a36ba9
Soudarjya <soudyg707@gmail.com>
2025-03-05 20:19:25 +0530

    private route

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,8 +1,8 @@
 import { Navigate, Outlet } from "react-router-dom";
 
 const PrivateRoute = ({ isAuthenticated }) => {
-  // return isAuthenticated ? <Outlet /> : <Navigate to="/" />;
-  return <Outlet />;
+  return isAuthenticated ? <Outlet /> : <Navigate to="/" />;
+  // return <Outlet />;
 };
 
 export default PrivateRoute;
