48bd08620de4672e2d3e589e391713e471ed9ae9
Soudarjya <soudyg707@gmail.com>
2025-03-17 19:25:50 +0530

    commit

---
+++ src/fleet_operations/fleet_operation_dashboard/FleetOperationDashboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './styles/fleetOperationDashboard'
+
+function fleetOperationDashboard() {
+  return (
+    <div>fleetOperationDashboard</div>
+  )
+}
+
+export default fleetOperationDashboard
